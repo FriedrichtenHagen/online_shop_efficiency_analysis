4a00a4cc1e52fe006b32afb2ac56a9eca0df89af
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2025-01-12 13:33:53 +0100

    Stop tracking logs/ and target/

--- transformation/target/run/online_shop_dbt/online_shop_dbt/models/example/my_second_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-
-
-  create or replace view `opensource-marketing-dwh`.`dbt_transformations_new`.`my_second_dbt_model`
-  OPTIONS()
-  as -- Use the `ref` function to select from other models
-
-select *
-from `facebook_ads_dimensions.ads`;
-
--- transformation/target/run/online_shop_dbt/online_shop_dbt/models/example/testing.sql
+++
@@ -1,6 +0,0 @@
-
-
-  create or replace view `opensource-marketing-dwh`.`dbt_transformations_new`.`testing`
-  OPTIONS()
-  as SELECT * FROM opensource-marketing-dwh.facebook_ads_dimensions.ad_sets;
-
